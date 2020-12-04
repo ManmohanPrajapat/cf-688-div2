53f5eb149b5bb95f03bf3532774813898ac48bdc
ManmohanPrajapat <52688363+ManmohanPrajapat@users.noreply.github.com>
2020-12-05 03:15:01 +0530

    Update E-snacks-graph.cpp

--- E-snacks-graph.cpp
+++ E-snacks-graph.cpp
@@ -10,6 +10,7 @@ int ans;
 int functor(int s,vector<int>adj[],int par)
 {
     vector<int>vect;
+    //basically going to every adjacent vertices to it except parent node
     for(int x:adj[s])
     {
         if(x!=par)
@@ -30,14 +31,19 @@ int functor(int s,vector<int>adj[],int par)
         ans=max(ans,vect[0]);
             return vect[0];
     }
+    //we can also do in n time but we can sort it so that we can save our time
     sort(vect.begin(),vect.end());
+    //largest branch
     int max1=vect.back();
     if(s==1)
     {
+        //ans will be either that or max length of any subtree or 1+2nd max 
         ans=max({ans,vect[vect.size()-2]+1LL,max1});
         return 0;
     }
+    // here max1+1 indicates maximum distance of that subtree to going to another subtree for the snack
     ans=max(ans,max1+1);
+    //return will be always the minimum value one
     return vect[0]+1;
 }
 void myfun()
